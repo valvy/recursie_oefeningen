28ab5db54752afd189179ab9df37c9b548ea27ca
heiko <heikovdheijden@hotmail.com>
2022-09-06 14:04:26 +0200

    Added Decimal to binary

--- 4_dec_binary.py
+++ 4_dec_binary.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 def convert_decimal_to_binary(decimal: int) -> str:
+    """
+    Converteer een decimaal getal recursief naar een binair getal.
+    Dat doe je door de volgende rekensom.
+    Is het getal oneven, haal er 1 van af en deel het door twee. (Bewaar de 1)
+    Is het getal even, haal er 0 van af en deel het door twee
+    Blijf doorgaan tot het getal 0 is.
+    Voorbeeld: 10
+    10 / 2 = 5, 10 is even dus 0
+    5 / 2 = 2, 5 is oneven dus 1
+    2 / 2 = 1, 1 is even dus 0
+    1 / 1 = 0, 1 is oneven dus 1
+    antwoord = 10 = 0101 (of 101 aangezien de 0 aan het begin niet meetelt)
+    """
     if decimal <= 1:
         return str(decimal)
-    else:
-        return convert_decimal_to_binary(decimal // 2) + str(decimal % 2)
+    return convert_decimal_to_binary(decimal // 2) + str(decimal % 2)
 
 
 def test_decimal_to_binary(getal, assertie):
